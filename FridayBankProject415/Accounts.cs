85d95e7b3c1a498fa47695dacc205823bf387a75
Your Name <you@email.com>
2016-04-15 17:25:33 -0400

    save point at eod

--- FridayBankProject415/Accounts.cs
+++ FridayBankProject415/Accounts.cs
@@ -11,16 +11,15 @@ namespace FridayBankProject415
     {
         //field
         private double clientBalance;
-        //If I cannot figure out a field to put here
-
-
-        //deposit into savings vs checking? Transfers?
-        //that would be fun, to have two. Maybe I will do that later.
+        //I cannot figure out another field to put here
+        //Create a random "is account valid" field
+        //for if account balance is not negative??? or account start date?
+        //Lauren said sure do whatever.
+        private bool validateAccount;
 
         //properties
         public double ClientBalance { get { return this.clientBalance; } set { this.clientBalance = value; } }
-        //public string StreamWriterDocPath { get { return this.streamWriterDocPath; } }//not really useful
-        //not sure about this doc path as a prop
+
 
         //methods
         public static string generateAcctNum()//METHOD
@@ -42,13 +41,11 @@ namespace FridayBankProject415
             using (StreamWriter outputFile = File.AppendText("AccountSummary.txt"))
             {
                 outputFile.Write(DateTime.Now);
-                outputFile.Write(" $" + depositAmt + " \tDeposited. \tCurrent Balance: \t$" + this.ClientBalance);
+                outputFile.Write(" $" + depositAmt + " \t+ \tCurrent Balance: \t$" + this.ClientBalance);
                 outputFile.Write("\r\n");
-                        }
-            //"Take the input and save it, change balance, also print to document (streamwriter)"
-
+            }
         }
-        public void Withdraw()
+        public void Withdraw()//METHOD
         {
             Console.WriteLine("\n\nHow much would you like to withdraw?");
             Double withdrawAmt = double.Parse(Console.ReadLine());
@@ -56,24 +53,20 @@ namespace FridayBankProject415
             {
                 Console.WriteLine("Your account does not hold that much money. Invalid amount.");
             }
-            else {
+            else
+            {
                 this.ClientBalance = this.ClientBalance - withdrawAmt;
                 Console.WriteLine("Your current balance is: $" + ClientBalance);
                 using (StreamWriter outputFile = File.AppendText("AccountSummary.txt"))
                 {
                     outputFile.Write(DateTime.Now);
-                    outputFile.Write(" $" + withdrawAmt + " \tWithdrawn. \tCurrent Balance: \t$" + this.ClientBalance);
+                    outputFile.Write(" $" + withdrawAmt + " \t- \tCurrent Balance: \t$" + this.ClientBalance);
                     outputFile.Write("\r\n");
                 }
             }
         }
-        public static void writeStuff()
-        {
-
-        }
 
         //Constructor - sets up client balance. 
-        //thinking in objects, we need an "account holder to hold this balance" I think.
         public Accounts(int cBalance)
         {
             this.ClientBalance = cBalance;
