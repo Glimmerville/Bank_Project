cba9c8a76c016a594710e084923df62bd8beefd1
Your Name <you@email.com>
2016-04-16 14:34:15 -0400

    Added error handling

--- FridayBankProject415/Accounts.cs
+++ FridayBankProject415/Accounts.cs
@@ -12,12 +12,11 @@ namespace FridayBankProject415
         //field
         private double clientBalance;
         private bool validateAccount;
+        private string Output { get; } = @"AccountSummary.txt";
 
         //properties
         public double ClientBalance { get { return this.clientBalance; } set { this.clientBalance = value; } }
-        public bool ValidateAccount {get {return this.validateAccount; } set { this.validateAccount = value; } }
-
-
+        public bool ValidateAccount { get { return this.validateAccount; } set { this.validateAccount = value; } }
         //methods
         public static string generateAcctNum()//METHOD
         {
@@ -35,11 +34,26 @@ namespace FridayBankProject415
             double depositAmt = double.Parse(Console.ReadLine());
             this.ClientBalance = this.ClientBalance + depositAmt;
             Console.WriteLine("Your current balance is: $" + ClientBalance);
-            using (StreamWriter outputFile = File.AppendText("AccountSummary.txt"))
+            try
+            { 
+                using (StreamWriter outputFile = File.AppendText(Output))
+                {
+                    outputFile.Write(DateTime.Now);
+                    outputFile.Write(" + $" + depositAmt + "\tUpdated Balance: $" + this.ClientBalance);
+                    outputFile.Write("\r\n");
+                }
+            }
+            catch (FileNotFoundException)
             {
-                outputFile.Write(DateTime.Now);
-                outputFile.Write(" + $" + depositAmt + "\tUpdated Balance: $" + this.ClientBalance);
-                outputFile.Write("\r\n");
+                Console.Error.WriteLine("Can't find file {0}", Output);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.Error.WriteLine("Invalid directory in the file path.");
+            }
+            catch(IOException)
+            {
+                Console.Error.WriteLine("Can't open the file {0}", Output);
             }
         }
         public void Withdraw()//METHOD
@@ -54,16 +68,31 @@ namespace FridayBankProject415
             {
                 this.ClientBalance = this.ClientBalance - withdrawAmt;
                 Console.WriteLine("Your current balance is: $" + ClientBalance);
-                using (StreamWriter outputFile = File.AppendText("AccountSummary.txt"))
+                try
                 {
-                    outputFile.Write(DateTime.Now);
-                    outputFile.Write(" - $" + withdrawAmt + "\tUpdated Balance: $" + this.ClientBalance);
-                    outputFile.Write("\r\n");
+                    using (StreamWriter outputFile = File.AppendText(Output))
+                    {
+                        outputFile.Write(DateTime.Now);
+                        outputFile.Write(" - $" + withdrawAmt + "\tUpdated Balance: $" + this.ClientBalance);
+                        outputFile.Write("\r\n");
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.Error.WriteLine("Can't find file {0}", Output);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.Error.WriteLine("Invalid directory in the file path.");
+                }
+                catch (IOException)
+                {
+                    Console.Error.WriteLine("Can't open the file {0}", Output);
                 }
             }
         }
 
-        //Constructor - sets up client balance. 
+        //Constructor - sets up client balance and validates client 
         public Accounts(int cBalance, bool cValidate)
         {
             this.ClientBalance = cBalance;
